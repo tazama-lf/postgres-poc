59aa25959e90a729ee962d888a7979e2c44558b9
rtkay123 <rodney.kanjala@sybrin.com>
2025-06-30 16:58:33 +0200

    feat(configuration): configuration ddl

---
+++ docker-stack/postgres-ddl/configuration.sql
@@ -0,0 +1,28 @@
+create table network_map (
+    id uuid primary key,
+    configuration jsonb not null
+);
+
+create table typology (
+    uuid uuid primary key,
+    configuration jsonb not null,
+    typologyId text generated always as (
+        configuration->'typologyId'
+    ) stored,
+    typologyCfg text generated always as (
+        configuration->'typologyCfg'
+    ) stored,
+    unique (typologyId, typologyCfg)
+);
+
+create table rule (
+    uuid uuid primary key,
+    configuration jsonb not null,
+    ruleId text generated always as (
+        configuration->'id'
+    ) stored,
+    ruleCfg text generated always as (
+        configuration->'cfg'
+    ) stored,
+    unique (ruleId, ruleCfg)
+);
